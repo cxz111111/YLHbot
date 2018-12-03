7370553eae24281f65063f925f05c750edcc64b9
David wang <wangzheng952@163.com>
2018-12-03 20:51:53 +0800

    Create info.py

---
+++ info.py
@@ -0,0 +1,27 @@
+youlike_id = ''
+youlike_password = ''
+youtube_id = ''
+youtube_password = ''
+teweet_id = ''
+teweet_password = ''
+#浏览器代理端口
+proxy = 'http://127.0.0.1:8087'
+
+youlike = "https://www.youlikehits.com"
+twitter = 'https://twitter.com/login'
+google = 'https://accounts.google.com/signin'
+
+y_view = 'https://www.youlikehits.com/youtubenew2.php'
+y_follow = 'https://www.youlikehits.com/youtube2.php'
+y_like = 'https://www.youlikehits.com/youtubelikes.php'
+
+t_follow = 'https://youlikehits.com/twitter2.php'
+t_like = 'https://youlikehits.com/favtweets.php'
+t_retweet = 'https://youlikehits.com/retweets.php'
+t_tweet = 'https://www.youlikehits.com/tweets.php'
+
+
+
+
+
+
