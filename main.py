8c66fea0eef6a63b891c1640944d9bc9f2e5f89a
David wang <wangzheng952@163.com>
2018-12-04 17:11:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,18 @@
 from info import *
 from subtwitter import get_twitter
 from subyoutube import get_youtube
-
+import sys
 from login import main_login
 
 if __name__ == '__main__':
     browser = main_login(youlike)
-    get_twitter(browser)
-    get_youtube(browser)
-    browser.close()
+    if(sys.argv[1]=='ty'):
+        get_twitter(browser)
+        get_youtube(browser)
+    if(sys.argv[1]=='t'):
+        get_twitter(browser)
+    if(sys.argv[1]=='y'):
+        get_youtube(browser)
+    else:
+        print('出错了')
+    browser.quit()
